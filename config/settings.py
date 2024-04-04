9e1e3afa23da260b05c71ebc45613a39c0167e43
JiYoungMin20 <ymji6922@gmail.com>
2024-04-04 10:44:19 +0900

    2nd commit

--- config/settings.py
+++ config/settings.py
@@ -76,10 +76,21 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "crowds",
+        "USER": "ymji6922",
+        "PASSWORD": "6922", 
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
 
